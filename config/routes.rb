4efe81aae570929fde40b6f5ade41db7860ecd47
Javier Zarate <zarate.javier.jz@gmail.com>
2020-05-26 11:13:26 -0600

    added animals

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root 'zoos#index'
-  resources :zoos
+  resources :zoos do 
+    resources :animals
+  end
 end
